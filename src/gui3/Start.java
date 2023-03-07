bf8db07a5ba071f1c1aca9955f0e057f3cf3056d
Maachou Imad <maachouimad1@gmail.com>
2023-03-07 11:39:24 +0100

    first commit

---
+++ src/gui3/Start.java
@@ -0,0 +1,13 @@
+package gui3;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// printing Hello world 
+		
+		System.out.println("Hello World ") ; 
+
+	}
+
+}
